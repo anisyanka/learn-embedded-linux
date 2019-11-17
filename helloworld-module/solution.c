db898f124d5f8dd3fe83b61efd2db00c618712c9
Anisyanka <anisimov.alexander.s@gmail.com>
2019-11-17 12:34:47 +0300

    [Hello-Module]: Test kmalloc() and kfree() kernel API functions (https://stepik.org/lesson/49216/step/4?unit=26939)

--- helloworld-module/solution.c
+++ helloworld-module/solution.c
@@ -1,27 +1,18 @@
 #include <linux/module.h>
+#include <linux/types.h>
+#include <linux/fcntl.h>
+#include <linux/fs.h>
+#include <linux/slab.h> /* kmalloc() */
+#include <linux/errno.h> 
+#include <linux/device.h> 
+#include <linux/init.h>
 
+/* external module file */
 #include <checker.h>
 
-#define CNT_CALLS_FROM_CHECKER_KO 10
-#define ARR_SIZE_FOR_CHECKER 2
-
-struct checker_arr {
-    short v[ARR_SIZE_FOR_CHECKER];
-    int s;
-};
-
-static struct checker_arr checkr_param[CNT_CALLS_FROM_CHECKER_KO] = {
-    {{ 0, 0 }, 0 },
-    {{ 0, 1 }, 1 },
-    {{ 0, 2 }, 2 },
-    {{ 0, 3 }, 3 },
-    {{ 0, 4 }, 4 },
-    {{ 0, 5 }, 5 },
-    {{ 0, 6 }, 6 },
-    {{ 0, 7 }, 7 },
-    {{ 0, 8 }, 8 },
-    {{ 0, 9 }, 9 }
-};
+static void *p1 = NULL;
+static int *p2 = NULL;
+static struct device *p3 = NULL;
 
 /*
  * This is the starting point of the kernel module's code execution.
@@ -29,39 +20,39 @@ static struct checker_arr checkr_param[CNT_CALLS_FROM_CHECKER_KO] = {
  */
 static int solution_init(void)
 {
-    int i;
-    int sum;
+    /* allocate any pointer type */
+    p1 = kmalloc((size_t)get_void_size(), GFP_KERNEL);
+    submit_void_ptr(p1);
 
-    CHECKER_MACRO;
+    /* allocate array of ints */
+    p2 = (int *)kmalloc(get_int_array_size(), GFP_KERNEL);
+    submit_int_array_ptr(p2);
 
-    for (i = 0; i < CNT_CALLS_FROM_CHECKER_KO; ++i) {
-        char out[100] = { 0 };
-
-        sum = array_sum(checkr_param[i].v, ARR_SIZE_FOR_CHECKER);
-        generate_output(sum, checkr_param[i].v, ARR_SIZE_FOR_CHECKER, out);
-
-        if (sum == checkr_param[i].s)
-            printk(KERN_INFO "%s", out);
-        else
-            printk(KERN_ERR "%s", out);
-
-    }
+    /* go mem for some device */
+    p3 = (struct device *)kmalloc(sizeof(struct device), GFP_KERNEL);
+    submit_struct_ptr(p3);
 
     return 0;
 }
-
-
+ 
 /*
  * This is the exit point of the kernel module. When the module is removed 
  * this function is called to do any sort of cleanup activities and other such
  * stuff.
  * 
- * For example you have made a tree where you keep someinformation - you would 
+ * For example you have made a tree where you keep some information - you would 
  * like to place the code for removing the nodes of the tree here.
  */
 static void solution_exit(void)
 {
-    CHECKER_MACRO;
+    if (p1)
+        checker_kfree(p1);
+
+    if (p2)
+        checker_kfree(p2);
+
+    if (p3)
+        checker_kfree(p3);
 }
 
 module_init(solution_init);
