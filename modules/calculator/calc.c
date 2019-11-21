8113f788c89869ab44c312d2142bc98ab378874a
Anisyanka <anisimov.alexander.s@gmail.com>
2019-11-21 18:22:05 +0300

    [Calc-Module]: demonstrate how to pass some args to kernel module

--- modules/calculator/calc.c
+++ modules/calculator/calc.c
@@ -3,57 +3,46 @@
  */
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
 #include <linux/moduleparam.h>
 
-struct sample {
-    int x, y;
-};
+/*
+ * Variable for integer parameter
+ */
+int x;
+module_param(x, int, 0);
 
-static void calculator(void) {
-    int *ptrx;
+int y;
+module_param(y, int, 0);
 
-    struct sample s;
-    s.x = 15;
-    s.y = 3;
-    ptrx = &s.x;
-
-    printk (KERN_INFO "x is %d y is %d\n", s.x, s.y);
-    printk (KERN_INFO "Sum is           %d\n", s.x + s.y);
-    printk (KERN_INFO "Difference is    %d\n", s.x - s.y);
-    printk (KERN_INFO "Product is       %d\n", s.x * s.y);
-    printk (KERN_INFO "Divison is       %d\n", s.x / s.y);
-    printk (KERN_INFO "Mod is           %d\n", s.x % s.y);
-    printk (KERN_INFO "Bitwise NOT of %d Is   %d\n", s.x, ~s.x);
-    printk (KERN_INFO "Bitwise OR is    %d\n", s.x | s.y);
-    printk (KERN_INFO "Bitwise AND is   %d\n", s.x & s.y);
-    printk (KERN_INFO "Bitwise XOR Is   %d\n", s.x ^ s.y);
-    printk (KERN_INFO "Logical OR Is    %d\n", s.x || s.y);
-    printk (KERN_INFO "Logical AND Is   %d\n", s.x && s.y);
-
-    if (s.x > s.y) {
-        printk (KERN_INFO "%d is greater than %d\n", s.x, s.y);
-    } else if (s.y > s.x) {
-        printk (KERN_INFO "%d is greater than %d\n", s.y, s.x);
-    } else {
-        printk (KERN_INFO "%d is equal to %d\n", s.y, s.x);
-    }
-
-    printk (KERN_INFO "Address of a is %p\n", ptrx);
-    printk (KERN_INFO "Value of ptrx is %d\n", *ptrx);
+static void calculator(int x, int y)
+{
+	printk(KERN_INFO "x is %d y is %d\n", x, y);
+	printk(KERN_INFO "Sum is           %d\n", x + y);
+	printk(KERN_INFO "Difference is    %d\n", x - y);
+	printk(KERN_INFO "Product is       %d\n", x * y);
+	printk(KERN_INFO "Divison is       %d\n", x / y);
+	printk(KERN_INFO "Mod is           %d\n", x % y);
+	printk(KERN_INFO "Bitwise NOT of %d Is   %d\n", x, ~x);
+	printk(KERN_INFO "Bitwise OR is    %d\n", x | y);
+	printk(KERN_INFO "Bitwise AND is   %d\n", x & y);
+	printk(KERN_INFO "Bitwise XOR Is   %d\n", x ^ y);
+	printk(KERN_INFO "Logical OR Is    %d\n", x || y);
+	printk(KERN_INFO "Logical AND Is   %d\n", x && y);
 }
 
 static int __init calc_init(void)
 {
-    printk(KERN_INFO "Hello from Anisyanka\n");
+	printk(KERN_INFO "Hello from Anisyanka\n");
 
-    calculator();
+	calculator(x, y);
 
-    return 0;
+	return 0;
 }
 
 static void __exit calc_exit(void)
 {
-    return;
+	return;
 }
 
 module_init(calc_init);
