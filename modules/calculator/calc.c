68b46008bac3a97645c7c1a780785087256c7982
Anisyanka <anisimov.alexander.s@gmail.com>
2019-11-22 13:34:28 +0300

    [Calc-Module]: export symbols for use in hello-module

--- modules/calculator/calc.c
+++ modules/calculator/calc.c
@@ -6,16 +6,10 @@
 #include <linux/kernel.h>
 #include <linux/moduleparam.h>
 
-/*
- * Variable for integer parameter
- */
-int x;
-module_param(x, int, 0);
+int GLOBAL_VARIABLE = 1000;
+EXPORT_SYMBOL(GLOBAL_VARIABLE);
 
-int y;
-module_param(y, int, 0);
-
-static void calculator(int x, int y)
+void calculator(int x, int y)
 {
 	printk(KERN_INFO "x is %d y is %d\n", x, y);
 	printk(KERN_INFO "Sum is           %d\n", x + y);
@@ -30,12 +24,11 @@ static void calculator(int x, int y)
 	printk(KERN_INFO "Logical OR Is    %d\n", x || y);
 	printk(KERN_INFO "Logical AND Is   %d\n", x && y);
 }
+EXPORT_SYMBOL(calculator);
 
 static int __init calc_init(void)
 {
-	printk(KERN_INFO "Hello from Anisyanka\n");
-
-	calculator(x, y);
+	printk(KERN_INFO "Hello from calculator module\n");
 
 	return 0;
 }
