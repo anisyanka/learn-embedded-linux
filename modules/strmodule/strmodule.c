eb61132a5ab239bc5b91129287f15c499c7ff6c5
Anisyanka <anisimov.alexander.s@gmail.com>
2019-11-24 12:58:41 +0300

    [str-module]: realize string funcs and test

--- modules/strmodule/strmodule.c
+++ modules/strmodule/strmodule.c
@@ -1,37 +1,116 @@
 #include <linux/init.h>
+#include <linux/slab.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/moduleparam.h>
 
 int mystring_find_length(const char *s)
 {
-	return 0;
+	int l = 0;
+
+	while (*s++ != '\0')
+		++l;
+
+	return l;
 }
 
 char *mystring_get_reverse(const char *srcstr, char *deststr)
 {
-	return NULL;
+	int i;
+	int src_len = strlen(srcstr) - 1; /* for last element */
+
+	for (i = src_len; i >= 0; i--)
+		deststr[src_len - i] = srcstr[i];
+
+	return deststr;
 }
 
 char *mystring_get_rotated(const char *srcstr, char *deststr,
 		int rotations, int direction)
 {
-	return NULL;
+	#define RIGTH_ROT_DIR 1
+	#define LEFT_ROT_DIR 0
+
+	int i;
+	int src_len = strlen(srcstr);
+
+	while (rotations >= src_len)
+		rotations -= src_len;
+
+	switch (direction) {
+	case RIGTH_ROT_DIR:
+		for (i = 0; i < rotations; ++i)
+			deststr[i] = srcstr[src_len - rotations + i];
+
+		for (; i < src_len; ++i)
+			deststr[i] = srcstr[i - rotations];
+		break;
+	case LEFT_ROT_DIR:
+		for (i = rotations; i < src_len; ++i)
+			deststr[i - rotations] = srcstr[i];
+
+		for (i = src_len - rotations; i < src_len; ++i)
+			deststr[i] = srcstr[i + rotations -src_len];
+		break;
+	default:
+		break;
+	}
+
+	return deststr;
 }
 
 int mystring_is_palindrome(const char *srcstr)
 {
-	return 0;
+	int ret;
+	int i;
+	int src_len = strlen(srcstr);
+	char *p = (char *)kmalloc(src_len + 1, GFP_KERNEL);
+
+	for (i = 0; i < src_len + 1; ++i)
+		p[i] = 0;
+
+	if (!p)
+		return 0;
+
+	mystring_get_reverse(srcstr, p);
+
+	if (strcmp(srcstr, p) == 0)
+		ret = 1;
+	else
+		ret = 0;
+
+	kfree(p);
+
+	return ret;
 }
 
 char *mystring_save_even_indx(const char *srcstr, char *deststr)
 {
-	return NULL;
+	int i, j;
+	int src_len = strlen(srcstr);
+
+	i = 0;
+	j = 0;
+	for (; i < src_len; ++i)
+		if (i % 2 == 0)
+			deststr[j++] = srcstr[i];
+
+	return deststr;
 }
 
 char *mystring_go_lowercase(const char *srcstr, char *deststr)
 {
-	return NULL;
+	int i = 0;
+	int src_len = strlen(srcstr);
+
+	for (; i < src_len; ++i) {
+		if (srcstr[i] < 65 || srcstr[i] > 90)
+			deststr[i] = srcstr[i];
+		else
+			deststr[i] = srcstr[i] + 32;	
+	}
+
+	return deststr;
 }
 
 EXPORT_SYMBOL(mystring_find_length);
--- modules/strmodule/strtest.c
+++ modules/strmodule/strtest.c
@@ -11,8 +11,118 @@ extern int mystring_is_palindrome(const char *srcstr);
 extern char *mystring_save_even_indx(const char *srcstr, char *deststr);
 extern char *mystring_go_lowercase(const char *srcstr, char *deststr);
 
+static const char d[] = "Hello world";
+static int dlen = sizeof(d) - 1;
+
+static int strlen_test(void)
+{
+	int my_len;
+
+	my_len = mystring_find_length(d);
+
+	if (my_len == dlen)
+		return 1;
+
+	return 0;
+}
+
+static int reverse_test(void)
+{
+	static char test[50] = { 0 };
+	static char dreverse[] = "dlrow olleH";
+
+	mystring_get_reverse(d, test);
+
+	if (strcmp(dreverse, test) == 0)
+		return 1;
+
+	return 0;
+}
+
+static int rotate_test(void)
+{
+	#define RIGTH_ROT_DIR 1
+	#define LEFT_ROT_DIR 0
+
+	static char test1[50] = { 0 };
+	static char test2[50] = { 0 };
+	static char drotate_right_by_3[] = "rldHello wo";
+	static char drotate_left_by_3[] = "lo worldHel";
+
+	mystring_get_rotated(d, test1, 3, RIGTH_ROT_DIR);
+	mystring_get_rotated(d, test2, 3, LEFT_ROT_DIR);
+
+	if (strcmp(drotate_right_by_3, test1) != 0)
+		return 0;
+
+	if (strcmp(drotate_left_by_3, test2) != 0)
+		return 0;
+
+	return 1;
+}
+
+static int palindrom_test(void)
+{
+	static const char palindrom[] = "Hello olleH";
+	static const char not_palindrom[] = "12345789";
+
+	if (mystring_is_palindrome(palindrom) == 0)
+		return 0;
+
+	if (mystring_is_palindrome(not_palindrom) == 1)
+		return 0;
+
+	return 1;
+}
+
+static int even_test(void)
+{
+	static char test[50] = { 0 };
+	static const char d[] = "Hello world";
+	static const char d_even_indx[] = "Hlowrd";
+
+	mystring_save_even_indx(d, test);
+
+	if (strcmp(d_even_indx, test) == 0)
+		return 1;
+
+	return 0;
+}
+
+static int lowercase_test(void)
+{
+	static char test[50] = { 0 };
+	static const char D[] = "HELLOWORLD";
+	static char Dlower[] = "helloworld";
+
+	mystring_go_lowercase(D, test);
+
+	if (strcmp(Dlower, test) == 0)
+		return 1;
+
+	return 0;
+}
+
 static int __init strtest_init(void)
 {
+	printk(KERN_INFO "strlen_test() state = %s",
+			strlen_test() ? "OK" : "FAIL");
+
+	printk(KERN_INFO "reverse_test() state = %s",
+			reverse_test() ? "OK" : "FAIL");
+
+	printk(KERN_INFO "rotate_test() state = %s",
+			rotate_test() ? "OK" : "FAIL");
+
+	printk(KERN_INFO "palindrom_test() state = %s",
+			palindrom_test() ? "OK" : "FAIL");
+
+	printk(KERN_INFO "even_test() state = %s",
+			even_test() ? "OK" : "FAIL");
+
+	printk(KERN_INFO "lowercase_test() state = %s",
+			lowercase_test() ? "OK\n" : "FAIL\n");
+
 	return 0;
 }
 
