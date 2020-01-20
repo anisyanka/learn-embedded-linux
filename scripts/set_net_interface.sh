3298c6c5f18ab24fe86a29765c4ae1a472c6bc96
anisyan <anisimov.alexander.s@gmail.com>
2020-01-21 00:44:45 +0300

    [scripts]: add info about how-to-install NFS server

--- scripts/set_net_interface.sh
+++ scripts/set_net_interface.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
+#
+# This interface will be UP when device with MAC=f8dc7a000001 was plugged
+#
 nmcli con add type ethernet ifname enxf8dc7a000001 ip4 192.168.0.1/24
+
+#
+# To install NFS server:
+# - Disable v4 into /etc/default/nfs-kernel-server with help `--no-nfs-version 4`
+# - edit the /etc/exports file as root to add the following lines
+#   /home/<user>/linux-kernel-labs/modules/nfsroot 192.168.0.100(rw,no_root_squash,no_subtree_check)
+# - sudo systemctl restart nfs-config
+# - sudo /etc/init.d/nfs-kernel-server restart
+#
+# - sudo chown nobody:nogroup /mnt/sharedfolder
+# - sudo chmod 777 /mnt/sharedfolder
+#
+
